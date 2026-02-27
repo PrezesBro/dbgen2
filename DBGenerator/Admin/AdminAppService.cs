f5b4dd4102be71d603b338e41ed839e7aab40d1f
Krystian Brożek <Krystian.Brozek@kogniter.com>
2026-02-27 04:46:16 +0100

    dodanie edycji reklam, dodanie edycji baz danych i tabel

---
+++ DBGenerator/Admin/AdminAppService.cs
@@ -0,0 +1,60 @@
+﻿using DBGenerator.Data;
+using DBGenerator.Models;
+using DBGenerator.Models.Ads;
+
+namespace DBGenerator.Admin
+{
+    public class AdminAppService : IAdminAppService
+    {
+        IDataFacade _data;
+        public AdminAppService(IDataFacade data) 
+        {
+            _data = data;
+        }
+
+        public async Task<Database> GetDatabase(int id)
+        {
+            return await _data.GetDatabase(id);
+        }
+
+        public async Task<Database> GetDatabaseWithTables(int id)
+        {
+            return await _data.GetDatabaseWithTables(id);
+        }
+
+        public async Task<List<Database>> GetDatabases()
+        {
+            return await _data.GetDatabases();
+        }
+
+        public async Task Save(Database db)
+        {
+            await _data.Save(db);
+        }
+
+        public async Task Clone(int id)
+        {
+            await _data.Clone(id);
+        }
+
+        public async Task<Table> GetTable(int id)
+        {
+            return await _data.GetTable(id);
+        }
+
+        public async Task Save(Table table)
+        {
+            await _data.Save(table);
+        }
+
+        public async Task<List<Ads>> GetAds()
+        {
+            return await _data.GetAllAds();
+        }
+
+        public async Task Save(Ads ads)
+        {
+            await _data.Save(ads);
+        }
+    }
+}
---
+++ DBGenerator/Models/AdminViewmodel.cs
@@ -0,0 +1,8 @@
+﻿namespace DBGenerator.Models
+{
+    public class AdminViewModel
+    {
+        public List<Database> Databases { get; set; }
+        public List<Ads.Ads> Ads { get; set; }
+    }
+}
